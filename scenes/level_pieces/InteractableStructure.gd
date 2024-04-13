35f4d125ecb49033eb60ca25b0f70d1c17fd4f95
Adam Giles <adamag@cox.net>
2024-04-12 23:53:30 -0400

    Going to do interactable stuff next

---
+++ scenes/level_pieces/InteractableStructure.gd
@@ -0,0 +1,18 @@
+class_name InteractableThing
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func start_interacting():
+	print("starting interaction")
+
+func interact_with():
+	print("interacted with")
--- scenes/units/Unit.gd
+++ scenes/units/Unit.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 
 
-enum state {IDLE, WALKING}
+enum state {IDLE, WALKING, INTERACTING}
 var interacting_with: Node = null
 
 func _ready():
@@ -16,8 +16,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		GameManager.command_active_units(Vector3(10, 0, 10), Globals.COMMAND.MOVE)
 	
+	velocity = Vector3.ZERO
+	
 	if not is_on_floor():
-		velocity.y -= 10 * delta
+		velocity.y -= 100 * delta
 		
 	if !nav_agent.is_navigation_finished():
 		calc_dir(delta)
@@ -27,6 +29,7 @@ func _physics_process(delta):
 func calc_dir(delta: float):
 	var target = nav_agent.get_next_path_position()
 	var dir = global_position.direction_to(target)
+	print(dir)
 	velocity = dir * speed
 
 func send_command(location: Vector3, type: Globals.COMMAND):
