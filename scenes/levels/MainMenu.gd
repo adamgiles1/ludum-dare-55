863efb78233a633278e7e6258397db5165b1fa10
Adam Giles <adamag@cox.net>
2024-04-12 21:23:49 -0400

    Actual stuff

---
+++ scenes/levels/MainMenu.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@onready var play_butt: Button = $%PlayButton
+@onready var options_butt: Button = $%OptionsButton
+@onready var exit_butt: Button = $%ExitButton
+@onready var menu_holder: Control = $%MenuHolder
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_butt.connect("pressed", play)
+	options_butt.connect("pressed", options)
+	exit_butt.connect("pressed", exit)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func play():
+	get_tree().change_scene_to_file("res://scenes/levels/LevelOne.tscn")
+
+func options():
+	switch_menu("Option")
+
+func exit():
+	pass
+
+func switch_menu(menu: String):
+	for child: Control in menu_holder.get_children():
+		child.visible = child.name == menu
