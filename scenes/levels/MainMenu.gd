9b43189a670e773799e91c1888ee4c68eb8780a5
Anson Rutherford <>
2024-04-15 19:48:02 -0400

    Main menu fix

--- scenes/levels/MainMenu.gd
+++ scenes/levels/MainMenu.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @onready var play_butt: Button = $%PlayButton
-@onready var exit_butt: Button = $%ExitButton
 @onready var back_butt: Button = $%BackButton
 @onready var menu_holder: Control = $%MenuHolder
 @onready var credits_butt: Button = $%CreditsButton
@@ -9,7 +8,6 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play_butt.connect("pressed", play)
-	exit_butt.connect("pressed", exit)
 	back_butt.connect("pressed", back)
 	credits_butt.connect("pressed", view_credits)
 
@@ -22,9 +20,6 @@ func play():
 
 func back():
 	switch_menu("Main")
-
-func exit():
-	pass
 
 func switch_menu(menu: String):
 	for child: Control in menu_holder.get_children():
